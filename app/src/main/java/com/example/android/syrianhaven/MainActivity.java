d7259720dfc38afd04dcad6cd27d1118b3cdd116
thedeadline <igiuong@gatech.edu>
2016-04-17 07:11:48 -0700

    adjusted look and feel and added backend magic so it only uses two activities that share data.

--- app/src/main/java/com/example/android/syrianhaven/MainActivity.java
+++ app/src/main/java/com/example/android/syrianhaven/MainActivity.java
@@ -9,33 +9,28 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String USA = "usa";
-    public static final String AUS = "aus";
-    public static final String DEU = "deu";
-    public static final String NORW = "nor"; // NOR is a reserved Java keyword
-    public static final String CDN = "cdn";
+public class MainActivity extends AppCompatActivity{
     //Define views
     private Button AusButton, UsaButton, CdnButton, DeuButton, NorButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //Find views
         UsaButton = (Button) findViewById(R.id.Usa_button);
         AusButton = (Button) findViewById(R.id.Aus_button);
         CdnButton = (Button) findViewById(R.id.Cdn_button);
         DeuButton = (Button) findViewById(R.id.Deu_button);
         NorButton = (Button) findViewById(R.id.Nor_button);
-
         //Assign listener to buttons
         UsaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Start next activity
-                Intent intent = new Intent(MainActivity.this, UsaActivity.class);
-
+                Bundle bundle = new Bundle();
+                bundle.putString("Country Code", "USA");
+                Intent intent = new Intent(MainActivity.this, CountryActivity.class);
+                intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
@@ -45,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 //Start next activity
                 Bundle bundle = new Bundle();
                 bundle.putString("Country Code", "AUS");
-                Intent intent = new Intent(MainActivity.this, AusActivity.class);
+                Intent intent = new Intent(MainActivity.this, CountryActivity.class);
                 intent.putExtras(bundle);
                 startActivity(intent);
             }
@@ -56,24 +51,32 @@ public class MainActivity extends AppCompatActivity {
                 Bundle bundle = new Bundle();
                 bundle.putString("Country Code", "CDN");
                 //Start next activity
-                Intent intent = new Intent(MainActivity.this, AusActivity.class);
+                Intent intent = new Intent(MainActivity.this, CountryActivity.class);
                 intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
-
-    }
-    /*
-private void assignListener(Button button){
-    button.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View v) {
-            //Start next activity
-            if(button.getId() == "Cdn_button"){
-            Intent intent = new Intent(MainActivity.this, AusActivity.class);
+        DeuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("Country Code", "DEU");
+                //Start next activity
+                Intent intent = new Intent(MainActivity.this, CountryActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
             }
-            startActivity(intent);
-        }
-    });
-}*/
+        });
+        NorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("Country Code", "NOR");
+                //Start next activity
+                Intent intent = new Intent(MainActivity.this, CountryActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+    }
 }
