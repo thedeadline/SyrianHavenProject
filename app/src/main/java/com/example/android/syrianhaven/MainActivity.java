05c12fd6bac984c8cf0abea2ed96d190a76a3524
thedeadline <igiuong@gatech.edu>
2016-04-17 06:07:10 -0700

    adjusted look and feel and added backend magic so it only uses two activities that share data.

--- app/src/main/java/com/example/android/syrianhaven/MainActivity.java
+++ app/src/main/java/com/example/android/syrianhaven/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.syrianhaven;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,28 +10,26 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String USA = "usa";
+    public static final String AUS = "aus";
+    public static final String DEU = "deu";
+    public static final String NORW = "nor"; // NOR is a reserved Java keyword
+    public static final String CDN = "cdn";
     //Define views
-    private Button AusButton;
-    private Button UsaButton;
+    private Button AusButton, UsaButton, CdnButton, DeuButton, NorButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Find views
-        AusButton = (Button) findViewById(R.id.Aus_button);
         UsaButton = (Button) findViewById(R.id.Usa_button);
+        AusButton = (Button) findViewById(R.id.Aus_button);
+        CdnButton = (Button) findViewById(R.id.Cdn_button);
+        DeuButton = (Button) findViewById(R.id.Deu_button);
+        NorButton = (Button) findViewById(R.id.Nor_button);
 
-        //Assign a listener to button
-        AusButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Start next activity
-                Intent intent = new Intent(MainActivity.this, AusActivity.class);
-                startActivity(intent);
-            }
-        });
+        //Assign listener to buttons
         UsaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +39,41 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+        AusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Start next activity
+                Bundle bundle = new Bundle();
+                bundle.putString("Country Code", "AUS");
+                Intent intent = new Intent(MainActivity.this, AusActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+        CdnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("Country Code", "CDN");
+                //Start next activity
+                Intent intent = new Intent(MainActivity.this, AusActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
 
+    }
+    /*
+private void assignListener(Button button){
+    button.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            //Start next activity
+            if(button.getId() == "Cdn_button"){
+            Intent intent = new Intent(MainActivity.this, AusActivity.class);
+            }
+            startActivity(intent);
+        }
+    });
+}*/
 }
